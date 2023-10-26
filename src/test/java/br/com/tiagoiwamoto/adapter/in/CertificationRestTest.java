6521793a20da852e18bcbd68fde52db9c0d17b7a
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2023-10-26 06:09:09 -0300

    Adicionando testes parciais do dominio certification

---
+++ src/test/java/br/com/tiagoiwamoto/adapter/in/CertificationRestTest.java
@@ -0,0 +1,88 @@
+package br.com.tiagoiwamoto.adapter.in;
+
+import br.com.tiagoiwamoto.core.usecase.CertificationUsecase;
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import mock.CertificationMock;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+@QuarkusTest
+class CertificationRestTest {
+
+    @Inject
+    private CertificationRest rest;
+    @InjectMock
+    private CertificationUsecase usecase;
+
+    @Test
+    void index() {
+        var dados = CertificationMock.generateDataDto();
+        Mockito.when(this.usecase.listarRegistros()).thenReturn(List.of(dados));
+        var resposta = this.rest.index();
+
+        Assertions.assertEquals(200, resposta.getStatus());
+        Assertions.assertEquals(dados.getUuid(), resposta.getEntity().get(0).getUuid());
+        Assertions.assertEquals(dados.getName(), resposta.getEntity().get(0).getName());
+        Assertions.assertEquals(dados.getEarnDate(), resposta.getEntity().get(0).getEarnDate());
+        Assertions.assertEquals(dados.getPathOfImage(), resposta.getEntity().get(0).getPathOfImage());
+        Assertions.assertEquals(dados.getPathOfImageThumb(), resposta.getEntity().get(0).getPathOfImageThumb());
+        Assertions.assertEquals(dados.getValidateUrl(), resposta.getEntity().get(0).getValidateUrl());
+        Assertions.assertEquals(dados.getCreatedAt(), resposta.getEntity().get(0).getCreatedAt());
+        Assertions.assertEquals(dados.getUpdatedAt(), resposta.getEntity().get(0).getUpdatedAt());
+        Mockito.verify(this.usecase, Mockito.times(1)).listarRegistros();
+    }
+
+    @Test
+    void create() throws IOException {
+        var dados = CertificationMock.generateDataDto();
+        Mockito.when(this.usecase.gravarRegistro(Mockito.any())).thenReturn(dados);
+        var resposta = this.rest.create(dados);
+
+        Assertions.assertEquals(201, resposta.getStatus());
+        Assertions.assertEquals(dados.getUuid(), resposta.getEntity().getUuid());
+        Assertions.assertEquals(dados.getName(), resposta.getEntity().getName());
+        Assertions.assertEquals(dados.getEarnDate(), resposta.getEntity().getEarnDate());
+        Assertions.assertEquals(dados.getPathOfImage(), resposta.getEntity().getPathOfImage());
+        Assertions.assertEquals(dados.getPathOfImageThumb(), resposta.getEntity().getPathOfImageThumb());
+        Assertions.assertEquals(dados.getValidateUrl(), resposta.getEntity().getValidateUrl());
+        Assertions.assertEquals(dados.getCreatedAt(), resposta.getEntity().getCreatedAt());
+        Assertions.assertEquals(dados.getUpdatedAt(), resposta.getEntity().getUpdatedAt());
+        Mockito.verify(this.usecase, Mockito.times(1)).gravarRegistro(Mockito.any());
+    }
+
+    @Test
+    void update() {
+        var dados = CertificationMock.generateDataDto();
+        Mockito.when(this.usecase.atualizarRegistro(Mockito.any())).thenReturn(dados);
+        var resposta = this.rest.update(dados);
+
+        Assertions.assertEquals(200, resposta.getStatus());
+        Assertions.assertEquals(dados.getUuid(), resposta.getEntity().getUuid());
+        Assertions.assertEquals(dados.getName(), resposta.getEntity().getName());
+        Assertions.assertEquals(dados.getEarnDate(), resposta.getEntity().getEarnDate());
+        Assertions.assertEquals(dados.getPathOfImage(), resposta.getEntity().getPathOfImage());
+        Assertions.assertEquals(dados.getPathOfImageThumb(), resposta.getEntity().getPathOfImageThumb());
+        Assertions.assertEquals(dados.getValidateUrl(), resposta.getEntity().getValidateUrl());
+        Assertions.assertEquals(dados.getCreatedAt(), resposta.getEntity().getCreatedAt());
+        Assertions.assertEquals(dados.getUpdatedAt(), resposta.getEntity().getUpdatedAt());
+        Mockito.verify(this.usecase, Mockito.times(1)).atualizarRegistro(Mockito.any());
+    }
+
+    @Test
+    void delete() {
+        Mockito.doNothing().when(this.usecase).deletarRegistro(Mockito.any());
+        var resposta = this.rest.delete(UUID.randomUUID());
+
+        Assertions.assertEquals(204, resposta.getStatus());
+        Mockito.verify(this.usecase, Mockito.times(1)).deletarRegistro(Mockito.any());
+    }
+
+
+}
---
+++ src/test/java/mock/CertificationMock.java
@@ -0,0 +1,46 @@
+package mock;
+
+import br.com.tiagoiwamoto.adapter.dto.CertificationDTO;
+import br.com.tiagoiwamoto.adapter.dto.ProfileDTO;
+import br.com.tiagoiwamoto.core.entity.CertificationEntity;
+import br.com.tiagoiwamoto.core.entity.ProfileEntity;
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.UUID;
+
+public class CertificationMock {
+
+    public static CertificationDTO generateDataDto(){
+        Faker faker = new Faker(new Locale("pt-BR"));
+        var data = new CertificationDTO();
+        data.setUuid(UUID.randomUUID());
+        data.setName(faker.name().fullName());
+        data.setEarnDate(LocalDate.now());
+        data.setPathOfImage("/files/mock/arquivo.png");
+        data.setPathOfImageThumb("/files/mock/arquivo_th.png");
+        data.setValidateUrl(faker.internet().url());
+        data.setId(faker.number().numberBetween(1L, 9999));
+        data.setCreatedAt(LocalDateTime.now());
+        data.setUpdatedAt(LocalDateTime.now());
+        return data;
+    }
+
+    public static CertificationEntity generateDataEntity(){
+        Faker faker = new Faker(new Locale("pt-BR"));
+        var data = new CertificationEntity();
+        data.setUuid(UUID.randomUUID());
+        data.setName(faker.name().fullName());
+        data.setEarnDate(LocalDate.now());
+        data.setPathOfImage("/files/mock/arquivo.png");
+        data.setPathOfImageThumb("/files/mock/arquivo_th.png");
+        data.setValidateUrl(faker.internet().url());
+        data.setId(faker.number().numberBetween(1L, 9999));
+        data.setCreatedAt(LocalDateTime.now());
+        data.setUpdatedAt(LocalDateTime.now());
+        return data;
+    }
+
+}
