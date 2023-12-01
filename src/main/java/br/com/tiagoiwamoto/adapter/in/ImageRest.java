cbe05222851e32ddb8c2bb944f05266b47361110
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2023-12-01 07:08:43 -0300

    Atualizando endpoint

--- src/main/java/br/com/tiagoiwamoto/adapter/in/ImageRest.java
+++ src/main/java/br/com/tiagoiwamoto/adapter/in/ImageRest.java
@@ -1,11 +1,8 @@
 package br.com.tiagoiwamoto.adapter.in;
 
-import br.com.tiagoiwamoto.core.facade.HomeFacade;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.RestQuery;
 
@@ -15,9 +12,6 @@ import java.io.FileNotFoundException;
 
 @Path(value = "/files")
 public class ImageRest {
-
-    @Inject
-    private HomeFacade facade;
 
     @GET
     @Produces({"image/png", "image/jpeg", "image/gif"})
@@ -30,14 +24,6 @@ public class ImageRest {
         } catch (FileNotFoundException e) {
             return Response.status(404).build();
         }
-    }
-
-    @GET
-    @Path(value = "/metrics")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response metrics(){
-        var response = this.facade.metrics();
-        return Response.ok(response).build();
     }
 
 }
