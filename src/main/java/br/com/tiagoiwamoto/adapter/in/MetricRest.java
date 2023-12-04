f036c2711e767042c6eba977c66ddd4ec0af868a
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2023-12-04 06:00:49 -0300

    Atualizando versão do app

---
+++ src/main/java/br/com/tiagoiwamoto/adapter/in/MetricRest.java
@@ -0,0 +1,23 @@
+package br.com.tiagoiwamoto.adapter.in;
+
+import br.com.tiagoiwamoto.core.facade.HomeFacade;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path(value = "/metrics")
+public class MetricRest {
+
+    @Inject
+    private HomeFacade facade;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response index(){
+        var response = this.facade.metrics();
+        return Response.ok(response).build();
+    }
+}
