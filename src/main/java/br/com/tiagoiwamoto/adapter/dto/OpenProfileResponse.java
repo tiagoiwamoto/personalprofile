ca7ed9ebed6807770fee1e5448e405772035c1c7
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2023-12-06 06:43:52 -0300

    Atualizando versão do app

--- src/main/java/br/com/tiagoiwamoto/adapter/dto/OpenProfileResponse.java
+++ src/main/java/br/com/tiagoiwamoto/adapter/dto/OpenProfileResponse.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Jacksonized
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
+@Builder
 public class OpenProfileResponse {
 
     private List<CertificationDTO> certifications;
